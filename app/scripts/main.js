bd118e52b4436603b6cd2eee98e63f3f4a47b054
Jess Scheuring <jscheuring@gmail.com>
2014-09-18 12:12:12 -0400

    implements flickr api using photos.search method

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,4 @@
-// var flickrApi = 'https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=a59e46bc559caf69b42be8464990c102&format=json&tags=bike,bicycles,bikes,bicycle';
-
-var flickrApi = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=bike,bicycles,bikes,bicycle';
+var flickrApi = 'https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=a59e46bc559caf69b42be8464990c102&format=json&tags=bike,bicycles,bikes,bicycle&json&per_page=3&jsoncallback=?';
 
 function renderTemplate(templateID, location, dataModel) {
   var templateString = $(templateID).text();
@@ -9,32 +7,13 @@ function renderTemplate(templateID, location, dataModel) {
   $(location).append(renderedTemplate);
 }
 
-var bikeArray = [];
-
-
 //attempting to get the data with a basic ajax request (doesn't work, but throws no errors??)
-// $.ajax({
-//   type: 'get',
-//   url: flickrApi
-// }).done(function(data) {
-//   console.log(data);
-//   _.each(data.items, function(data) {
-//     bikeArray.push({'bikePic': ""+ data.media.m});
-//   });
-//   _.times(3, function(n) {
-//     console.log(bikeArray[n]);
-//     renderTemplate('#templates-bicycle-pics', '.bicycle-pics', bikeArray[n]);
-//   });
-// });
-
-
-//attempting to get the data with a $.getJSON format
-$.getJSON(flickrApi, function(data) {
-  _.each(data.items, function(data) {
-    var imgURL = data.media.m.slice(0, data.media.m.length -5) + "z.jpg";
-    bikeArray.push({'bikePic': imgURL});
-  });
-  _.times(3, function(n) {
-    renderTemplate('#templates-bicycle-pics', '.bicycle-pics', bikeArray[n]);
+$.ajax({
+  type: 'GET',
+  url: flickrApi,
+  dataType: 'json'
+}).done(function(data) {
+  _.each(data.photos.photo, function(data) {
+    renderTemplate('#templates-bicycle-pics', '.bicycle-pics', data);
   });
 });

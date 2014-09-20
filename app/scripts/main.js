b15f6738df8b7c2ec8c1b234ae552943f9c81124
Jess Scheuring <jscheuring@gmail.com>
2014-09-20 18:45:08 -0400

    re-organizes js code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -64,20 +64,6 @@ Flicker API code below:
 
 var flickrApi = 'https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=a59e46bc559caf69b42be8464990c102&format=json&tags=bicycles,bicycle&per_page=3&extras=last_update&jsoncallback=?';
 
-
-
-
-/* ==========================================================================
-Underscore Render Template code below:
-========================================================================== */
-
-function renderTemplate(templateID, location, dataModel) {
-  var templateString = $(templateID).text();
-  var templateFunction = _.template(templateString);
-  var renderedTemplate = templateFunction(dataModel);
-  $(location).append(renderedTemplate);
-}
-
 //attempting to get the data with a basic ajax request (doesn't work, but throws no errors??)
 $.ajax({
   type: 'GET',
@@ -94,3 +80,16 @@ $.ajax({
     console.log('swipe');
   });
 });
+
+
+
+/* ==========================================================================
+Underscore Render Template code below:
+========================================================================== */
+
+function renderTemplate(templateID, location, dataModel) {
+  var templateString = $(templateID).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(dataModel);
+  $(location).append(renderedTemplate);
+}
